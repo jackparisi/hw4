730c567218dcf248a8456ec1f9bec9ae912e76fc
Jack Parisi <jackparisi202@gmail.com>
2020-10-10 16:25:34 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+var score = 0;
+var start = document.querySelector(".btn btn-success");
+var timeDisplay = document.querySelector("#time-left");
+var scoreDisplay = document.querySelector("#score-count");
+timeClock = 90
+var quiz = [
+{
+ title: "Commonly used data types DO NOT include",
+ choices: ["Strings", "Numbers", "Booleans", "Alerts"],
+ answer: "Alerts"
+},
+{
+    title: "Arrays in Javascript can be used to store",
+    choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"],
+    answer: "All of the Above"
+},
+{
+   title: "String values must be enclosed in ____ to when being assigned to variables",
+   choices: ["Commas", "Curly Brackets", "Quotes", "All of the Above"],
+   answer: "Quotes"
+},
+{
+    title: "The condition in an if else statement is enclosed in ___",
+    choices: ["Quotes", "Parentheses", "Curly Brackets", "Square Brackets"], 
+    answer: "Parentheses"
+},
+{
+    title: "A very useful tool used for development and debugging for printing content to the debugger is",
+    choices: ["Javascript", "Inspector", "Console Log", "Terminal"],
+    answer: "Console Log"
+}];
+    
+
+start.addEventListener("click", function(){
+   
+ });  
+
+for(i=0;i<quiz.length;i++){
+ console.log(quiz.title)   
+}
